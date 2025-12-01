06d231052ebcb534a144cf7d5cfd88e56c29d008
Samira08 <samireatluhanova@gmail.com>
2025-12-01 15:10:08 +0100

    added get method

--- src/main/java/com/example/devopsintegration/DevopsIntegrationApplication.java
+++ src/main/java/com/example/devopsintegration/DevopsIntegrationApplication.java
@@ -2,9 +2,15 @@ package com.example.devopsintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DevopsIntegrationApplication {
+
+	@GetMapping
+	public String message(){
+		return "succes deploy to k&8";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsIntegrationApplication.class, args);
